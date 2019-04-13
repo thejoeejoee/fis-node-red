8f757098d848e551723f9d9145ef02f75a188d1d
Josef Kolář <thejoeejoee@gmail.com>
2019-04-14 00:30:31 +0200

    Packages improves && settings

--- settings.js
+++ settings.js
@@ -3,5 +3,16 @@ module.exports = {
         page: {
             title: "FIS",
         }
+    },
+    adminAuth: {
+        type: "credentials",
+        users: [{
+            username: "fis",
+            password: "$2a$08$kjZA6I8imGRbv5AiCMAQe.hPwq1HQBQtq0NO.TbJWS4XpUWaOu8Hi",
+            permissions: "*",
+        }],
+        default: {
+            permissions: "read"
+        }
     }
 };
